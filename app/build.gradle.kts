5dd926804f208b0fd32020d25ef76a872e413ad1
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-08-31 12:04:43 +0300

    Prepare version 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.silverlightning.mycycle"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         vectorDrawables {
             useSupportLibrary = true
